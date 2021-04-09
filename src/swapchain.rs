9904be04ef6a57a14a54f66c4d95fa76bf2c90b7
Rajat Rajput <rajputrajat@gmail.com>
2021-04-09 22:08:48 +0530

    correct it

--- src/swapchain.rs
+++ src/swapchain.rs
@@ -145,7 +145,7 @@ impl State {
                 vertex: VertexState {
                     module: &vs_module,
                     entry_point: "main",
-                    buffers: &[Vertex::desc()],
+                    buffers: &[],
                 },
                 fragment: Some(FragmentState {
                     module: &fs_module,
@@ -194,7 +194,7 @@ impl State {
                     vertex: VertexState {
                         module: &vs_module,
                         entry_point: "main",
-                        buffers: &[],
+                        buffers: &[Vertex::desc()],
                     },
                     fragment: Some(FragmentState {
                         module: &fs_module,
